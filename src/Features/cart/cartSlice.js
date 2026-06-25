c0abfb4ec2c21e2c9d474413c654ddce3a0b37c0
isveckrali <mehmet.can.seyhan1@gmail.com>
2026-06-25 23:51:11 +0200

    increase, descrease funcs done

--- src/Features/cart/cartSlice.js
+++ src/Features/cart/cartSlice.js
@@ -1,24 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 import cartItems from "../../cartItems";
 
-const initialState= {
-    cartItems: cartItems,
-    amount: 4,
-    total: 0,
-    isLoading: true
-}
+const initialState = {
+  cartItems: cartItems,
+  amount: 4,
+  total: 0,
+  isLoading: true,
+};
 
 const cartSlice = createSlice({
-    name: "cart",
-    initialState,
-    reducers:{
-        clearCart: (state) => {
-            state.cartItems = []
-        },
+  name: "cart",
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
     },
-})
 
-//console.log(cartSlice)
-export const { clearCart } = cartSlice.actions;
+    removeItem: (state, action) => {
+      const itemId = action.payload;
 
-export default cartSlice.reducer
+      state.cartItems = state.cartItems.filter(
+        (item) => item.id !== itemId
+      );
+    },
+
+    increase : (state, {payload}) => {
+      const cartItem = state.cartItems.find((item)=>item.id === payload.id)
+      cartItem.amount = cartItem.amount + 1
+    },
+
+    descrease : (state, {payload}) => {
+      const cartItem = state.cartItems.find((item)=>item.id === payload.id)
+      cartItem.amount = cartItem.amount - 1
+    },
+
+  },
+});
+
+export const { clearCart, removeItem, increase, descrease } = cartSlice.actions;
+
+export default cartSlice.reducer;
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,29 +1,50 @@
-import React from 'react'
+import React from "react";
 import { ChevronDown, ChevronUp } from "../icons";
+import { removeItem, increase, descrease } from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
 
 const CartItem = ({ id, img, title, price, amount }) => {
+  const dispatch = useDispatch();
+
   return (
-    <article className='cart-item'>
+    <article className="cart-item">
       <img src={img} alt={title} />
+
       <div>
         <h4>{title}</h4>
-        <h4 className='item-price'>${price}</h4>
-        <button className='remove-btn'>remove</button>
+        <h4 className="item-price">${price}</h4>
+
+        <button
+          className="remove-btn"
+          onClick={() => {
+            dispatch(removeItem(id));
+          }}
+        >
+          remove
+        </button>
       </div>
 
       <div>
-        <button className='amount-btn'>
+        <button className="amount-btn" onClick={() => {
+          dispatch(increase({ id }))
+        }}>
           <ChevronUp />
         </button>
 
-        <p className='amount'>{amount}</p>
+        <p className="amount">{amount}</p>
 
-        <button className='amount-btn'>
+        <button className="amount-btn" onClick={() => {
+          if (amount===1) {
+            dispatch(removeItem(id))
+            return
+          }
+          dispatch(descrease({ id }))
+        }}>
           <ChevronDown />
         </button>
       </div>
     </article>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
